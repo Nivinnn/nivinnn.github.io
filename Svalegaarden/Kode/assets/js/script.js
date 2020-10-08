5a4ead9698491fc89d8223a13f254a60e270c6aa
Nivinnn <1081551@ucn.dk>
2020-10-08 17:11:29 +0200

    h

---
+++ Svalegaarden/Kode/assets/js/script.js
@@ -0,0 +1,17 @@
+
+document.getElementById('headline').innerHTML = "NIVIN TAYEB";
+// document.getElementById('headline').style.fontSize = "xxx-large";
+
+
+function onChange(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+
+document.getElementById("darkmode").style.backgroundColor = "#bc9b8f";
+document.getElementById("darkmode").style.border = "none";
+document.getElementById("darkmode").style.fontFamily = "barlow";
+document.getElementById("darkmode").style.fontSize = "small";
+document.getElementById("darkmode").style.outline = "none";
+
